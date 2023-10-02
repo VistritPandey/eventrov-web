4a426c1d214298d506e48fc7acf02945b0168ddc
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2023-10-02 18:51:34 -0400

    k

--- src/Access.js
+++ src/Access.js
@@ -1,38 +1,21 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
 const callouts = [
     {
-      name: 'Desk and Office',
-      description: 'Work from home accessories',
+      name: 'For Event Planners',
+      description: 'Find the best artists, DJs and performers. Discover perfect venues based on your preference. Book trusted service providers/vendors for lighting, sound and other essentials.',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-      href: '#',
+      href: 'https://eventrov.com/contact-us#85f2e26a-f168-4699-add7-3a23ba5c9f9a',
     },
     {
-      name: 'Self-Improvement',
-      description: 'Journals and note-taking',
+      name: 'For Artists and Performers',
+      description: 'Showcase your talent to a vast network of event planners. Get booked for events that match your style and preferences. Build connections without the need for a manager',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-      href: '#',
+      href: 'https://forms.gle/VrxMKkbx9HfTHF1q7',
     },
     {
-      name: 'Travel',
-      description: 'Daily commute essentials',
+      name: 'For Venues and Vendors/Service Providers',
+      description: 'List your venue or services for free and get discovered by the right event planners. Manage bookings and inquiries effortlessly. Increase your visibility using SEO ',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-      href: '#',
+      href: 'https://forms.gle/R5JJJ6mX47W9CWQM8',
     },
   ]
   
@@ -41,7 +24,7 @@ const callouts = [
       <div className="bg-gray-100">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-            <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+            <h2 className="text-2xl font-bold text-gray-900">Get Early Access</h2>
   
             <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
               {callouts.map((callout) => (
