2e22afd912a83281f4116d5bbc8ff78c021b153a
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2023-10-04 12:44:51 -0400

    k

--- src/Access.js
+++ src/Access.js
@@ -21,28 +21,28 @@ const callouts = [
   
   export default function Access() {
     return (
-      <div className="bg-gray-100">
+      <div className="bg-black">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-            <h2 className="text-2xl font-bold text-gray-900">Get Early Access</h2>
-  
+            <h2 className="text-4xl font-bold text-indigo-600">Get Early Access</h2>
+            <p></p>
             <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
               {callouts.map((callout) => (
                 <div key={callout.name} className="group relative">
-                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-black sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
                     <img
                       src={callout.imageSrc}
                       alt={callout.imageAlt}
                       className="h-full w-full object-cover object-center"
                     />
                   </div>
-                  <h3 className="mt-6 text-sm text-gray-500">
+                  <h3 className="mt-6 text-sm text-indigo-400">
                     <a href={callout.href}>
                       <span className="absolute inset-0" />
                       {callout.name}
                     </a>
                   </h3>
-                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                  <p className="text-base font-semibold text-white">{callout.description}</p>
                 </div>
               ))}
             </div>
