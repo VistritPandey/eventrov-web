f8aa0788c1376697c625e62f043360cfe89d7d17
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2023-10-04 12:42:07 -0400

    k

--- src/Feature.js
+++ src/Feature.js
@@ -29,14 +29,15 @@ const features = [
 
 export default function Feature() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-black py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">About Us</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="text-5xl font-semibold leading-7 text-indigo-600">About Us</h2>
+          <p></p>
+          {/*<p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Everything you need for an event
-          </p>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          </p> */}
+          <p className="mt-6 text-lg leading-8 text-white">
           Eventrov is a platform that connects event organizers, venues, vendors, and artists in a seamless, user-friendly online environment leveraging advanced search and recommendation algorithms to match event organizers with ideal providers
           </p>
         </div>
@@ -44,13 +45,13 @@ export default function Feature() {
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
             {features.map((feature) => (
               <div key={feature.name} className="relative pl-16">
-                <dt className="text-base font-semibold leading-7 text-gray-900">
+                <dt className="text-base font-semibold leading-7 text-indigo-400">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
                   </div>
                   {feature.name}
                 </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                <dd className="mt-2 text-base leading-7 text-white">{feature.description}</dd>
               </div>
             ))}
           </dl>
