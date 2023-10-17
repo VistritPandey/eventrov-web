b4432f5a57544731c5c9c67278bb13f7fdce6e7c
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2023-10-17 17:42:40 -0400

    k

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Header from './Header';
 import Footer from './Footer';
 import Contact from './Contact';
 import Testimonial from './Testimonial';
+import Login from './Login';
+import Dashboard from './Dashboard';
 //import Pricing from './Pricing';
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/feature" element={<Feature />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
           <Route path="/testimonial" element={<Testimonial />} />
           <Route path="/services" element={<Access />} />
           <Route path="/contact" element={<Contact />} />
