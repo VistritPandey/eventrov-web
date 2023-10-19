6dba9cf04990851d80409b0d99231b97c42457cc
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2023-10-19 16:15:11 -0400

    k

--- src/Feed.js
+++ src/Feed.js
@@ -3,8 +3,16 @@ import { IoCreateOutline } from "react-icons/io5";
 import { VscSend } from "react-icons/vsc";
 import { auth, db } from "./Firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { getFirestore, collection, query, getDocs, where, addDoc } from "firebase/firestore";
-//import Post from "./Post";
+import {
+  getFirestore,
+  collection,
+  query,
+  getDocs,
+  where,
+  addDoc,
+} from "firebase/firestore";
+import Post from "./Post";
+import "./Feed.css";
 
 function Feed() {
   const [user, loading, error] = useAuthState(auth);
@@ -12,11 +20,34 @@ function Feed() {
   const [posts, setPosts] = useState([]);
   const [feedImage, setfeedImage] = useState(""); // Corrected variable name
 
+  // Function to fetch posts from Firebase
+  const fetchPosts = async () => {
+    const postsCollection = collection(db, "posts");
+    const postsQuery = query(postsCollection);
+
+    const querySnapshot = await getDocs(postsQuery);
+    const fetchedPosts = [];
+
+    querySnapshot.forEach((doc) => {
+      fetchedPosts.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+
+    setPosts(fetchedPosts);
+  };
+
+  useEffect(() => {
+    // Fetch posts from Firebase when the component mounts
+    fetchPosts();
+  }, []); // The empty dependency array ensures it runs once after initial render
+
   const sendPost = async (e) => {
     e.preventDefault();
-      
-    if (user && input.trim() !== '') {
-      const postsRef = collection(db, "posts"); // Get the reference to the "posts" collection
+
+    if (user && input.trim() !== "") {
+      const postsRef = collection(db, "posts");
       await addDoc(postsRef, {
         name: user.displayName,
         description: "description",
@@ -25,35 +56,37 @@ function Feed() {
         image: feedImage,
         timestamp: Date.now(),
       });
-  
+
       setInput("");
       setfeedImage("");
+      
+      // Fetch posts again after adding a new post
+      fetchPosts();
     }
   };
 
   return (
-    <div className="bg-white rounded-lg p-4 shadow-md mb-4">
-      <div className="flex items-center mb-4">
-        <IoCreateOutline className="text-2xl text-gray-500" />
-        <form className="flex-grow ml-4">
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            type="text"
-            className="w-full p-2 border rounded-lg focus:outline-none"
-            placeholder="Post something"
-          />
-        </form>
-        <button
-          onClick={sendPost}
-          type="submit"
-          className="p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600"
-        >
-          <VscSend className="text-2xl" />
-        </button>
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <IoCreateOutline />
+          <form>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+              placeholder="Post something"
+            />
+            <button onClick={sendPost} className="light" type="submit">
+              <VscSend className="lightblue" />
+            </button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          {/* Your input options here */}
+        </div>
       </div>
-      <div className="feed__inputOptions">
-        {/*{posts.map(
+      {posts.map(
         ({ id, data: { name, message, photoUrl, timestamp, image } }) => (
           <Post
             key={id}
@@ -64,8 +97,7 @@ function Feed() {
             image={image}
           />
         )
-      )} */}
-      </div>
+      )}
     </div>
   );
 }
