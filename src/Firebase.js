4db7b0e403fda3215071f23d9e18216c5ba1e8e4
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2023-10-29 17:47:38 -0400

    k

--- src/Firebase.js
+++ src/Firebase.js
@@ -35,6 +35,15 @@ const firebaseConfig = {
     measurementId: "G-NS3RK3MZDN"
 };
 
+// apiKey: process.env.REACT_API_KEY,
+// authDomain: process.env.REACT_AUTH_DOMAIN,
+// databaseURL: process.env.REACT_DATABASE_URL,
+// projectId: process.env.REACT_PROJECT_ID,
+// storageBucket: process.env.REACT_STORAGE_BUCKET,
+// messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+// appId: process.env.REACT_APP_ID,
+// measurementId: process.env.REACT_MEASUREMENT_ID,
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
