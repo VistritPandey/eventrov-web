2556bee414f60e4b2c2bafd863c54fc43b627f37
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2023-11-15 15:28:02 -0500

    Add image modal and state for showing/hiding it

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,6 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import InputOption from "./InputOption";
-import "./Post.css";
 import {
   BsFillHandThumbsUpFill,
   BsFillChatLeftFill,
@@ -15,16 +14,15 @@ import {
   collection,
   getDocs,
   where,
-  Timestamp, // Import Timestamp
+  Timestamp,
 } from "firebase/firestore";
 
 function Post({ name, message, photoUrl, timestamp, image }) {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
+  const [showImageModal, setShowImageModal] = useState(false);
 
-  // Function to format timestamp
   function formatTimestamp(timestamp) {
-    // Assuming timestamp is a Firestore Timestamp object
     const dateInMillis = timestamp.toMillis();
     const date = new Date(dateInMillis);
     return date.toLocaleString("en-US", {
@@ -38,30 +36,53 @@ function Post({ name, message, photoUrl, timestamp, image }) {
     });
   }
 
+  const handleImageClick = () => {
+    setShowImageModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowImageModal(false);
+  };
+
   return (
-    <div className="post">
-      <div className="post__header">
-        <Avatar src={"https://robohash.org/"+name} />
-        <div className="post__info">
-          <h2>{name}</h2>
-          <p>{formatTimestamp(timestamp)}</p> {/* Format the timestamp */}
+    <div className="post bg-gray-800 p-4 mb-4 rounded">
+      <div className="post__header flex items-center mb-4">
+        <Avatar src={"https://robohash.org/" + name} />
+        <div className="post__info ml-4 text-white">
+          <h2 className="text-lg">{name}</h2>
+          <p className="text-sm text-gray-500">{formatTimestamp(timestamp)}</p>
         </div>
       </div>
-      <div className="post__body">
-        <p>{message}</p>
+      <div className="post__body text-white overflow-wrap-anywhere mb-4">
+        <p className="text-base">{message}</p>
       </div>
-      <div className="post__image">
-        <img src={image} alt="" />
+      {image && (
+        <div className="post__image mb-4 cursor-pointer" onClick={handleImageClick}>
+          <img src={image} alt="" className="rounded w-full h-64 object-cover" />
+        </div>
+      )}
+      <div className="post__buttons flex justify-evenly">
+        <InputOption Icon={BsFillHandThumbsUpFill} title="Like" color="gray" cursor="pointer" />
+        <InputOption Icon={BsFillChatLeftFill} title="Comment" color="lightgreen" cursor="pointer" />
+        <InputOption Icon={BsShareFill} title="Share" color="orange" cursor="pointer" />
       </div>
-      <div className="post__buttons">
-        <InputOption Icon={BsFillHandThumbsUpFill} title="Like" color="gray" />
-        <InputOption
-          Icon={BsFillChatLeftFill}
-          title="Comment"
-          color="lightgreen"
-        />
-        <InputOption Icon={BsShareFill} title="Share" color="orange" />
-      </div>
+      {showImageModal && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center">
+          <div className="max-w-screen-md w-full overflow-hidden">
+            <img
+              src={image}
+              alt=""
+              className="rounded-lg max-w-full max-h-full"
+            />
+            <button
+              onClick={handleCloseModal}
+              className="absolute top-4 right-4 text-white p-2 rounded-full bg-gray-800"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
