f2a6c4908801b10fd30f40f274e2adf8e8fdbedb
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2023-11-21 15:21:07 -0500

    k

--- src/Header.js
+++ src/Header.js
@@ -45,7 +45,7 @@ function Header() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="/login" className="text-sm font-semibold leading-6 text-white">
+            <a href="https://social.eventrov.com" className="text-sm font-semibold leading-6 text-white">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
@@ -86,7 +86,7 @@ function Header() {
                 </div>
                 <div className="py-6">
                   <a
-                    href="/login"
+                    href="https://social.eventrov.com"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white"
                   >
                     Log in
